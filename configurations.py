b7bc9a94caf4f53d1af6053dea74721018104b23
Sachin Malhotra <sachinma@usc.edu>
2018-09-11 14:58:11 -0700

    Updated configurations

--- configurations.py
+++ configurations.py
@@ -1,7 +1,7 @@
 class Config:
-    eval_using = "accuracy"
-    relative_train_path = '/data/arxiv/train.dat'
-    relative_dev_path = '/data/arxiv/valid.dat'
+    eval_using = "loss"
+    relative_train_path = '/data/85%/arxiv/train.dat'
+    relative_dev_path = '/data/85%/arxiv/valid.dat'
     min_freq = 1
     emsize = 512
     hidden_size = 1150
@@ -11,66 +11,54 @@ class Config:
     max_grad_norm = 10
     d1 = 0
     d2 = 0
-    epochs = 40
-    patience = 12
+    epochs = 100
+    patience = 20
     input_dropout = 0
     pretrained = None
     weight_decay = 0
     bidirectional = True
 
     # Attention based scoring configurations
-    use_attention = True
-    attention_score_type = "sigmoid"
+    use_attention = False
+    attention_score_type = "softmax"
     attention_type = "weighted"
-
-class D1Config_1(Config):
-    d1 = 1
-
-class D1Config_2(Config):
-    d1 = 2
 
 class D2Config_1(Config):
     d2 = 1
 
-class D2Config_1WeightDecay(Config):
+class D2_1_L2(Config):
     d2 = 1
+    weight_decay = 0.9
 
-class D2Config_1Dropout(Config):
+class D2_1_L2_Sigmoid_Weighted(D2_1_L2):
     d2 = 1
-    input_dropout = 0.2
+    use_attention = True
+    attention_score_type = "sigmoid"
+    attention_type = "weighted"
 
-class D2Config_2(Config):
-    d2 = 2
-
-class D1D2Config_1(Config):
-    d1 = 1
+class D2_1_L2_Sigmoid_Concat(D2_1_L2):
     d2 = 1
+    use_attention = True
+    attention_score_type = "sigmoid"
+    attention_type = "concat"
 
-class D1D2Config_2(Config):
-    d1 = 2
-    d2 = 2
+class D2_1_L2_Softmax_Weighted(D2_1_L2):
+    d2 = 1
+    use_attention = True
+    attention_score_type = "softmax"
+    attention_type = "weighted"
 
-class D1D2Dropout(D1D2Config_1):
-    input_dropout = 0.2
-
-class D1D2Pretrained(D1D2Config_1):
-    pretrained = 'embeddings/arxiv/embedding.vec'
-
-class D1D2PretrainedDropout(D1D2Config_1):
-    pretrained = 'embeddings/arxiv/embedding.vec'
-    input_dropout = 0.2
+class D2_1_L2_Softmax_Concat(D2_1_L2):
+    d2 = 1
+    use_attention = True
+    attention_score_type = "softmax"
+    attention_type = "concat"
 
 def get_conf(conf):
     return config[conf]
 
-config = {"d1_1" : D1Config_1(),
-          "d1_2" : D1Config_2(),
-          "d2_1" : D2Config_1(),
-          "d2_1_drop": D2Config_1Dropout(),
-          "d2_1_L2": D2Config_1WeightDecay(),
-          "d2_2" : D2Config_2(),
-          "d1d2_1" : D1D2Config_1(),
-          "d1d2_2" : D1D2Config_2(),
-          "d1d2drop" : D1D2Dropout(),
-          "d1d2pre" : D1D2Pretrained(),
-          "d1d2predrop" : D1D2PretrainedDropout()}
+config = {"d2_1" : D2_1_L2(),
+          "sig_weight": D2_1_L2_Sigmoid_Weighted(),
+          "sig_concat": D2_1_L2_Sigmoid_Concat(),
+          "softmax_weight": D2_1_L2_Softmax_Weighted(),
+          "softmax_concat": D2_1_L2_Softmax_Concat()}
